8a298f09454b66de52482e50f858720275c1d9d9
danijeljw-rpc <danijel@repasscloud.com>
2022-08-11 19:31:01 +0200

    using go, nginx, .net core

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+echo "Angler-Bamboo-Bunkbed-Silliness6" | sudo -S ls
+
+if [ ! -d "/data/caddy" ] 
+then
+    sudo mkdir -p /data/caddy
+fi
+cp ./caddy/Caddyfile /data/caddy/Caddyfile
+
+
+if [ ! -d "/data/caddy/data" ] 
+then
+    sudo mkdir -p /data/caddy/data
+fi
+
+if [ ! -d "/data/caddy/config" ] 
+then
+    sudo mkdir -p /data/caddy/config
+fi
+
+docker compose down && git pull && docker rmi $(docker images -aq) && docker compose up -d
